aee0b6ee1b8396bcb795828ba6476e58cdbab8b6
evan <evanslight@live.com>
2018-10-08 16:21:38 +0800

    assert with corrected ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -2,34 +2,61 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
-class Car {
-    park() {
-        return 'stopped';
-    }
-
-    drive() {
-        return 'vroom';
-    }
-}
+const { interface, bytecode } = require('../compile');
+//class Car {
+//    park() {
+//        return 'stopped';
+//    }
+//
+//    drive() {
+//        return 'vroom';
+//    }
+//}
 
 // make the car global so that describe can access it
-let car;
+//let car;
+let accounts;
+let inbox;
 
 // before each it run this  
-beforeEach(() => {
-    car = new Car();
+beforeEach(async () => {
+//    car = new Car();
+    // Get a list of all accounts
+//    web3.eth.getAccounts()
+//        .then(fetchedAccounts => {
+//            console.log(fetchedAccounts);
+//        });
+    accounts = await web3.eth.getAccounts();
+
+    //use one of those accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there']})
+        .send({ from: accounts[0], gas: '1000000' })
+
+    inbox.setProvider(provider);
 });
 
-describe('Car Class', () => {
-    it('can park', () => {
-        const car = new Car();
-        assert.equal(car.park(), 'stopped');
+describe('smart contract', () => {
+//    it('can park', () => {
+//        const car = new Car();
+//        assert.equal(car.park(), 'stopped');
+//    });
+//
+//    it('can drive', () => {
+//        const car = new Car();
+//        assert.equal(car.drive(), 'vroom');
+//    });
+
+    it('deploys a contract', () => {
+        assert.ok(inbox.options.address);
     });
 
-    it('can drive', () => {
-        const car = new Car();
-        assert.equal(car.drive(), 'vroom');
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there');
     });
+
 });
